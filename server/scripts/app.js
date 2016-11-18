62a863daa1ca196eb0a74d099ba5058202dc9ae5
croe <crow.uc@gmail.com>
2016-11-19 07:15:48 +0900

    デザイン反映

--- server/scripts/app.js
+++ server/scripts/app.js
@@ -46,6 +46,39 @@
 		console.log(bignameId, twitterId);
 	});
 
+	var ctx = $('#graph');
+	var data = {
+    labels: ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Neuroticism"],
+    datasets: [
+        {
+            label: "Target",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90, 81, 56]
+        },
+        {
+            label: "You",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            data: [28, 48, 40, 19, 96]
+        }
+    ]
+};
+	var radar = new Chart(ctx,{
+    type: 'radar',
+    data: data
+    // options: options
+});
+
+
 	// showData(testdata);
 	function showData(data) {
 		var $head = $('.js-tsumenoaka-head'),
